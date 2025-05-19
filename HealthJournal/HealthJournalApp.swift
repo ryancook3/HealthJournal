d2c0a4b34eb8a76e03b8d17d7052099b858f90dc
Ryan <ryancook96@yahoo.com>
2025-05-19 15:49:25 -0700

    Initial Commit

---
+++ HealthJournal/HealthJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthJournalApp.swift
+//  HealthJournal
+//
+//  Created by Ryan Cook on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthJournalTests/HealthJournalTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthJournalTests.swift
+//  HealthJournalTests
+//
+//  Created by Ryan Cook on 5/19/25.
+//
+
+import Testing
+@testable import HealthJournal
+
+struct HealthJournalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
